102f895070f932376ae258bd8c944dd5a12c1477
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-05-04 17:59:34 +0600

    food item class

--- users.py
+++ users.py
@@ -21,19 +21,26 @@ class Admin(User):
     def add_employee(self,restaurent,employee):
         restaurent.add_employee(employee)
 
-    def viewEmployee(self,restaurent):
+    def view_employee(self,restaurent):
         restaurent.view_employee()
+
+    def add_new_item(self,restaurent,item):
+        restaurent.menu.add_menu_item(item)
+
+    def delete_item(self,restaurent,item):
+        restaurent.menu.remove_item(item)
 
 
 class Restaurent:
     def __init__(self,name):
         self.name = name
-        self.employees = [] 
+        self.employees = []
+        self.menu = FoodItem()
 
     def add_employee(self,employee):
         self.employees.append(employee)
 
-    def viewEmployee(self):
+    def view_employee(self):
         print("Employee List -")
         for emp in self.employees:
             print(emp.name,emp.email,emp.phone,emp.address)
@@ -59,3 +66,23 @@ class Menu:
             print("Item Deleted!!")
         else :
             print("Item Not Found!!")
+
+    def show_menu(self):
+        print("**********Menu**********")
+        print("Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f'{item.name}\t{item.price}\t{item.quantity}')
+
+
+class FoodItem:
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+item = FoodItem("Pizza",150,12)
+menu = Menu()
+
+menu.add_menu_item(item)
+menu.show_menu()
